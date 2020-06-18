764207994c3bd88d70c8c4c637b423e222a044f6
svijay77 <sarvagnyav@gmail.com>
2020-06-18 15:34:06 -0700

    changed output display from embed to text when reloading cogs

--- mini_akniight.py
+++ mini_akniight.py
@@ -2,21 +2,27 @@ import data as d
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
 
 
 load_dotenv()
 TOKEN = d.DISCORD_TOKEN
-GUILD = ""
 
 bot = commands.Bot(command_prefix = ".")
 
-@bot.command(help = "Loads a cog of your choice")
-async def load(ctx, extension):
-    bot.load_extension("cogs.{}".format(extension))
+@bot.event
+async def on_ready():
+    print("{} has connected to discord!".format(bot.user))
+    await bot.change_presence(activity = discord.Game("admiring Kanye West"))
+    bot.load_extension("cogs.leaks")
+    bot.load_extension("cogs.lyrics")
 
 @bot.command(help = "Reloads a cog of your choice")
 async def reload(ctx, extension):
     bot.unload_extension("cogs.{}".format(extension))
     bot.load_extension("cogs.{}".format(extension))
+    await ctx.send("{} has been reloaded!".format(extension))
 
 bot.run(TOKEN)
